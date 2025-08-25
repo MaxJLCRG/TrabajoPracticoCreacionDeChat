dfd09c363faa1c578158cae5f907623b0db241fd
MaxJLCRG <maximojimenezjl@gmail.com>
2025-08-25 08:43:52 -0300

    25/08 - mini avance

---
+++ proyecto-chat/src/app/Login/page.js
@@ -0,0 +1,12 @@
+"use client"
+
+import Form from "../Components/Form"
+
+export default function Login(){
+    return(
+        <>
+            <h1>Titulo</h1>
+            <Form></Form>
+        </>
+    )
+}
